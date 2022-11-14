cf77055c9c627f75844706ae62e751e961afdde9
JensDiemer <git@jensdiemer.de>
2022-11-14 08:28:00 +0100

    fix template

--- poetry-python/{{cookiecutter.package_name}}/{{cookiecutter.package_name}}/tests/test_project_setup.py
+++ poetry-python/{{cookiecutter.package_name}}/{{cookiecutter.package_name}}/tests/test_project_setup.py
@@ -3,18 +3,20 @@ from unittest import TestCase
 import tomli
 from bx_py_utils.path import assert_is_file
 
+import {{ cookiecutter.package_name }}
 from {{ cookiecutter.package_name }} import __version__
 
 
-PACKAGE_ROOT = Path(__file__).parent.parent
+PACKAGE_ROOT = Path({{ cookiecutter.package_name }}.__file__).parent.parent
 
 class ProjectSetupTestCase(TestCase):
 
     def test_version(self):
-        self.assertIsNotNone(__version__)
-
         pyproject_toml_path = Path(PACKAGE_ROOT, 'pyproject.toml')
         assert_is_file(pyproject_toml_path)
+
+        self.assertIsNotNone(__version__)
+
         pyproject_toml = tomli.loads(pyproject_toml_path.read_text(encoding='UTF-8'))
         pyproject_version = pyproject_toml['tool']['poetry']['version']
 
