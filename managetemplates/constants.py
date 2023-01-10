eb0d309fac0d698f574417184b537bdab456a0ad
JensDiemer <git@jensdiemer.de>
2023-01-10 09:21:13 +0100

    Use constants.ALL_TEMPLATES

--- managetemplates/constants.py
+++ managetemplates/constants.py
@@ -16,3 +16,10 @@ assert_is_file(REQ_TXT_PATH)
 # dependencies + "tests"-optional-dependencies:
 REQ_DEV_TXT_PATH = PACKAGE_ROOT / 'managetemplates' / 'requirements.dev.txt'
 assert_is_file(REQ_DEV_TXT_PATH)
+
+ALL_TEMPLATES = (
+    'pipenv-python',
+    'piptools-python',
+    'poetry-python',
+    'yunohost_django_package',
+)
