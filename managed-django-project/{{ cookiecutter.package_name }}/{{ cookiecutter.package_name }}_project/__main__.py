9dd833e3fa7beeebfbc84fd450631cd98b206986
JensDiemer <git@jensdiemer.de>
2023-04-05 14:55:03 +0200

    Udpate managed-django-project to manage_django_project v0.3.0

--- managed-django-project/{{ cookiecutter.package_name }}/{{ cookiecutter.package_name }}_project/__main__.py
+++ managed-django-project/{{ cookiecutter.package_name }}/{{ cookiecutter.package_name }}_project/__main__.py
@@ -2,13 +2,7 @@
     Allow {{ cookiecutter.package_name }} to be executable
     through `python -m {{ cookiecutter.package_name }}`.
 """
-from pathlib import Path
-
-from manage_django_project.config import ManageConfig
 from manage_django_project.manage import execute_django_from_command_line
-
-import {{ cookiecutter.package_name }}
-import {{ cookiecutter.package_name }}_project
 
 
 def main():
@@ -16,14 +10,7 @@ def main():
     entrypoint installed via pyproject.toml and [project.scripts] section.
     Must be set in ./manage.py and PROJECT_SHELL_SCRIPT
     """
-    execute_django_from_command_line(
-        config=ManageConfig(
-            module={{ cookiecutter.package_name }}_project,
-            project_root_path=Path({{ cookiecutter.package_name }}.__file__).parent.parent,
-            local_settings='{{ cookiecutter.package_name }}_project.settings.local',
-            test_settings='{{ cookiecutter.package_name }}_project.settings.tests',
-        )
-    )
+    execute_django_from_command_line()
 
 
 if __name__ == '__main__':
