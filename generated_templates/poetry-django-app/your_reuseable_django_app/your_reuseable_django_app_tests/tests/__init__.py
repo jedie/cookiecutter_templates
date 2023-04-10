68c680b0f82fcd807d5719cfd2aa6878d3c9a12d
Jens Diemer <git@jensdiemer.de>
2023-04-10 15:51:09 +0200

    Hacky way to expand the failed test output (#62)

--- generated_templates/poetry-django-app/your_reuseable_django_app/your_reuseable_django_app_tests/tests/__init__.py
+++ generated_templates/poetry-django-app/your_reuseable_django_app/your_reuseable_django_app_tests/tests/__init__.py
@@ -1,6 +1,12 @@
 from pathlib import Path
+import os
+import unittest.util
 
 import your_reuseable_django_app
+
+
+# Hacky way to expand the failed test output:
+unittest.util._MAX_LENGTH = os.environ.get('UNITTEST_MAX_LENGTH', 300)
 
 
 PACKAGE_ROOT = Path(your_reuseable_django_app.__file__).parent.parent
