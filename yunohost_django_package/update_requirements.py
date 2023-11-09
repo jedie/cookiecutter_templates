4d8ee5a42adc834994d20e187be15af6dff33044
JensDiemer <git@jensdiemer.de>
2023-11-09 20:02:26 +0100

    Update and run yunohost_django_package/update_requirements.py

--- yunohost_django_package/update_requirements.py
+++ yunohost_django_package/update_requirements.py
@@ -4,6 +4,8 @@
 
         .../cookiecutter_templates$ cli.py update-template-req
 """
+import sys
+
 from bx_py_utils.path import assert_is_dir
 from cli_base.cli_tools.subprocess_utils import verbose_check_call
 
@@ -15,14 +17,14 @@ def main(verbose):
     pkg_path = PACKAGE_ROOT / 'generated_templates' / 'yunohost_django_package' / 'django_example_ynh'
     assert_is_dir(pkg_path)
 
-    verbose_check_call('make', 'update', cwd=pkg_path, verbose=verbose)
+    verbose_check_call(sys.executable, 'dev-cli.py', 'update', cwd=pkg_path, verbose=verbose)
 
     # Sync the requirements back to the cookiecutter template:
     template_path = PACKAGE_ROOT / 'yunohost_django_package' / '{{ cookiecutter.ynh_app_pkg_name }}'
     print('_' * 100)
     verbose_copy2(
-        src=pkg_path / 'poetry.lock',
-        dst=template_path / 'poetry.lock',
+        src=pkg_path / 'requirements.dev.txt',
+        dst=template_path / 'requirements.dev.txt',
     )
     print('_' * 100)
     verbose_copy2(
