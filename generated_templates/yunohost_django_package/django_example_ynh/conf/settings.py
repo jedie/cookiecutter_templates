ece7d590f74fabe45c58c7ea6d5320ffb5bbbee3
Mr. Test <foo-bar@test.tld>
2025-10-31 22:29:18 +0100

    Update generated templates

--- generated_templates/yunohost_django_package/django_example_ynh/conf/settings.py
+++ generated_templates/yunohost_django_package/django_example_ynh/conf/settings.py
@@ -82,9 +82,16 @@ AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
 )
 
-LOGIN_URL = '/yunohost/sso/'
-LOGIN_REDIRECT_URL = '/yunohost/sso/'
-LOGOUT_REDIRECT_URL = '/yunohost/sso/'
+# SSOwat should be used for login and should redirect back to the YunoHost App.
+# Use SSOwatLoginRedirectView for that:
+LOGIN_URL = 'ssowat-login'
+
+# After login, redirect back to the YunoHost App:
+if PATH_URL:
+    LOGIN_REDIRECT_URL = f'/{PATH_URL}/'
+else:
+    # Installed to domain root, without a path prefix:
+    LOGIN_REDIRECT_URL = '/'
 
 ROOT_URLCONF = 'urls'  # .../conf/urls.py
 
