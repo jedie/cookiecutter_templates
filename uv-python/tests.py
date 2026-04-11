00221d55bb0f65857cc679f58184d9592d098346
Mr. Test <foo-bar@test.tld>
2026-04-11 13:07:27 +0200

    use assert_in()

--- uv-python/tests.py
+++ uv-python/tests.py
@@ -1,4 +1,5 @@
 from bx_py_utils.path import assert_is_file
+from cli_base.cli_tools.test_utils.assertion import assert_in
 
 from managetemplates.tests.base import PackageTestBase, TempGitRepo
 
@@ -18,14 +19,14 @@ class UvPythonTemplateTestCase(PackageTestBase):
             self.assert_is_executeable(cli_bin)
 
             output = self.test_project.check_output(cli_bin, 'version')
-            self.assert_in('your_cool_package v0.0.1', output)
+            assert_in(content=output, parts=('your-cool-package v0.0.1',))
 
             assert_is_file(self.pkg_path / '.venv-app' / 'bin' / 'pip')
             assert_is_file(self.pkg_path / '.venv-app' / 'bin' / 'python')
             assert_is_file(self.pkg_path / '.venv-app' / 'bin' / 'your_cool_package')
 
             output = self.test_project.check_output(cli_bin, '--help')
-            self.assert_in('usage: your_cool_package [-h]', output)
+            assert_in(content=output, parts=('usage: your-cool-package [-h]',))
 
             ############################################################################
             # Bootstrap by call the ./dev-cli.py
@@ -34,7 +35,7 @@ class UvPythonTemplateTestCase(PackageTestBase):
             self.assert_is_executeable(dev_cli_bin)
 
             output = self.test_project.check_output(dev_cli_bin, 'version')
-            self.assert_in('your_cool_package v0.0.1', output)
+            assert_in(content=output, parts=('your_cool_package v0.0.1',))
 
             assert_is_file(self.pkg_path / '.venv' / 'bin' / 'pip')
             assert_is_file(self.pkg_path / '.venv' / 'bin' / 'python')
@@ -46,24 +47,43 @@ class UvPythonTemplateTestCase(PackageTestBase):
             assert_is_file(self.pkg_path / '.venv' / 'bin' / 'your_cool_package')  # The normal CLI
 
             output = self.test_project.check_output(dev_cli_bin, '--help')
-            self.assert_in('usage: ./dev-cli.py [-h]', output)
-            self.assert_in(' lint ', output)
-            self.assert_in(' coverage ', output)
-            self.assert_in(' publish ', output)
-            self.assert_in(' update-readme-history ', output)
+            assert_in(
+                content=output,
+                parts=(
+                    'usage: ./dev-cli.py [-h]',
+                    ' lint ',
+                    ' coverage ',
+                    ' publish ',
+                    ' update-readme-history ',
+                ),
+            )
 
             output = self.test_project.check_output(dev_cli_bin, 'nox', '--help')
-            self.assert_in('usage: nox [-h]', output)
-            self.assert_in('List all available sessions', output)
-            self.assert_in('.venv/bin/nox --help', output)
+            assert_in(
+                content=output,
+                parts=(
+                    'usage: nox [-h]',
+                    'List all available sessions',
+                    '.venv/bin/nox --help',
+                ),
+            )
 
             output = self.test_project.check_output(dev_cli_bin, 'nox', '-l')
-            self.assert_in('tests-3.13', output)
-            self.assert_in('tests-3.12', output)
-            self.assert_in('.venv/bin/nox -l', output)
+            assert_in(
+                content=output,
+                parts=(
+                    'tests-3.13',
+                    'tests-3.12',
+                    '.venv/bin/nox -l',
+                ),
+            )
 
             output = self.test_project.check_output(dev_cli_bin, 'lint')
-            self.assert_in('All checks passed!', output)
+            try:
+                assert_in(content=output, parts=('ruff', 'All checks passed!'))
+            except Exception:
+                temp_git.display_git_diff()
+                raise
 
             output = self.test_project.check_output(
                 dev_cli_bin,
@@ -72,7 +92,7 @@ class UvPythonTemplateTestCase(PackageTestBase):
                     GITHUB_ACTION='1',  # Disable README history test ;)
                 ),
             )
-            self.assert_in('Ran 8 tests', output)
+            assert_in(content=output, parts=('Ran 8 tests',))
 
             # The project unittests checks also the code style and tries to fix them,
             # in this case, we have a code difference:
