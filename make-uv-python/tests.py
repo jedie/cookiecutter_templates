214f320a528bf7155654b6d37cd326b24b830246
Mr. Test <foo-bar@test.tld>
2026-04-11 13:07:27 +0200

    use assert_in()

--- make-uv-python/tests.py
+++ make-uv-python/tests.py
@@ -2,6 +2,7 @@ import subprocess
 from pathlib import Path
 
 from bx_py_utils.path import assert_is_file
+from cli_base.cli_tools.test_utils.assertion import assert_in
 
 from managetemplates.tests.base import PackageTestBase, TempGitRepo
 
@@ -16,7 +17,7 @@ class MakeUvPythonTemplateTestCase(PackageTestBase):
 
             if not Path(self.pkg_path / '.venv' / 'bin' / 'ruff').exists():
                 output = self.test_project.check_output('make', 'install')
-                self.assert_in('uv sync', output)
+                assert_in(content=output, parts=('uv sync',))
 
             assert_is_file(self.pkg_path / '.venv' / 'bin' / 'pip')
             assert_is_file(self.pkg_path / '.venv' / 'bin' / 'python')
@@ -28,8 +29,7 @@ class MakeUvPythonTemplateTestCase(PackageTestBase):
 
             output = self.test_project.check_output('make', 'lint', exit_on_error=True)
             try:
-                self.assertIn('ruff', output, f'Output was:\n{output}')
-                self.assertIn('All checks passed!', output, f'Output was:\n{output}')
+                assert_in(content=output, parts=('ruff', 'All checks passed!'))
             except Exception:
                 temp_git.display_git_diff()
                 raise
@@ -37,7 +37,7 @@ class MakeUvPythonTemplateTestCase(PackageTestBase):
             subprocess.check_call(['make', 'lint'], cwd=self.pkg_path)
 
             output = self.test_project.check_output('make', 'test')
-            self.assert_in('Ran 4 test', output)
+            assert_in(content=output, parts=('Ran 4 test',))
 
             # The project unittests checks also the code style and tries to fix them,
             # in this case, we have a code difference:
